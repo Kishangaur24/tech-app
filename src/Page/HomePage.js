2e5622e934cbc12940c930701270b15fbd3bd9eb
Kishangaur24 <114826113+Kishangaur24@users.noreply.github.com>
2023-12-26 16:42:07 +0530

    partner

--- src/Page/HomePage.js
+++ src/Page/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Carousel from '../components/Carousel/Carousel'
+import Placement from '../components/Placement/Placement'
 
 const HomePage = () => {
   return (
     <div>
       <Header/>
       <Carousel/>
+      <Placement/>
     </div>
   )
 }
---
+++ src/components/Placement/Placement.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import style from "./Placement.module.css"
+const Placement = () => {
+  return (
+    <div>
+      <h1>Placement Partner</h1>
+      <div className={style.main}>
+        <div className={style.company}>
+            <img src='https://cdn-icons-png.flaticon.com/128/5968/5968269.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/5977/5977581.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/5968/5968382.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/882/882725.png' height="100px"/>
+        </div>
+        <div className={style.company}>
+            <img src='https://cdn-icons-png.flaticon.com/128/5968/5968852.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/882/882706.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969238.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/9018/9018508.png' height="100px"/>
+        </div>
+        <div className={style.company}>
+            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969038.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969043.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/732/732178.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969323.png' height="100px"/>
+        </div>
+        <div className={style.company}>
+            <img src='https://cdn-icons-png.flaticon.com/128/882/882603.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969139.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969096.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/5968/5968349.png' height="100px"/>
+        </div>
+      
+      </div>
+    </div>
+  )
+}
+
+export default Placement
