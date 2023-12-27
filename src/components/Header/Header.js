26793ebbce2bbb54066748cb941a9f701c918533
Kishangaur24 <114826113+Kishangaur24@users.noreply.github.com>
2023-12-27 19:54:58 +0530

    update image size

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,13 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
-
+import style from "./Header.module.css"
 function BasicExample() {
   return (
     <Navbar bg="dark" expand="lg" variant="dark" >
       <Container>
         <Navbar.Brand to="/home">
-            <img src='https://zeblearn.com/best-sap-training-in-india/assets/images/zeblearn_logo_new_02.jpg' height="45px"/>
+            <img className={style.logo} src='https://zeblearn.com/best-sap-training-in-india/assets/images/zeblearn_logo_new_02.jpg' />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
--- src/components/Placement/Placement.js
+++ src/components/Placement/Placement.js
@@ -6,28 +6,28 @@ const Placement = () => {
       <h1>Placement Partner</h1>
       <div className={style.main}>
         <div className={style.company}>
-            <img src='https://cdn-icons-png.flaticon.com/128/5968/5968269.png' height="100px"/>
-            <img src='https://cdn-icons-png.flaticon.com/128/5977/5977581.png' height="100px"/>
-            <img src='https://cdn-icons-png.flaticon.com/128/5968/5968382.png' height="100px"/>
-            <img src='https://cdn-icons-png.flaticon.com/128/882/882725.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/5968/5968269.png' />
+            <img src='https://cdn-icons-png.flaticon.com/128/5977/5977581.png' />
+            <img src='https://cdn-icons-png.flaticon.com/128/5968/5968382.png' />
+            <img src='https://cdn-icons-png.flaticon.com/128/882/882725.png' />
         </div>
         <div className={style.company}>
-            <img src='https://cdn-icons-png.flaticon.com/128/5968/5968852.png' height="100px"/>
-            <img src='https://cdn-icons-png.flaticon.com/128/882/882706.png' height="100px"/>
-            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969238.png' height="100px"/>
-            <img src='https://cdn-icons-png.flaticon.com/128/9018/9018508.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/5968/5968852.png' />
+            <img src='https://cdn-icons-png.flaticon.com/128/882/882706.png' />
+            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969238.png' />
+            <img src='https://cdn-icons-png.flaticon.com/128/9018/9018508.png' />
         </div>
         <div className={style.company}>
-            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969038.png' height="100px"/>
-            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969043.png' height="100px"/>
-            <img src='https://cdn-icons-png.flaticon.com/128/732/732178.png' height="100px"/>
-            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969323.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969038.png' />
+            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969043.png' />
+            <img src='https://cdn-icons-png.flaticon.com/128/732/732178.png' />
+            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969323.png' />
         </div>
         <div className={style.company}>
-            <img src='https://cdn-icons-png.flaticon.com/128/882/882603.png' height="100px"/>
-            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969139.png' height="100px"/>
-            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969096.png' height="100px"/>
-            <img src='https://cdn-icons-png.flaticon.com/128/5968/5968349.png' height="100px"/>
+            <img src='https://cdn-icons-png.flaticon.com/128/882/882603.png' />
+            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969139.png' />
+            <img src='https://cdn-icons-png.flaticon.com/128/5969/5969096.png' />
+            <img src='https://cdn-icons-png.flaticon.com/128/5968/5968349.png' />
         </div>
       
       </div>
